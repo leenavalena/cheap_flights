c445c0469aedf5774e0e4ae74fce2284d7277b69
Valena <valenadelvalle4@gmail.com>
2019-08-11 02:18:29 -0400

    commit

--- cheap_flights.gemspec
+++ cheap_flights.gemspec
@@ -33,4 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "nokogiri", ">= 1.0"
   spec.add_development_dependency "colorize", '~>0.8.1'
+  spec.add_development_dependency "pry"
 end
--- lib/cheap_flights/flights.rb
+++ lib/cheap_flights/flights.rb
@@ -1,2 +1,26 @@
-
+class CheapFlights::Flights 
+    attr_accessor :location, :airline, :price, :url
     
+    def self.available 
+        # Scrape theflightdeal and return flights based on the data 
+        self.scrape_flights
+
+      
+    end
+
+    def self.scrape_flights
+        flights = []
+
+        flights << self.scrape_flightdeal 
+        # go to theflightdeal, find the specials
+        # extract the data 
+        # instantiate a flight
+
+        flights
+    end
+
+    def self.scrape_flightdeal
+        doc = Nokogiri::HTML(open("https://www.theflightdeal.com/"))
+        binding.pry
+    end
+end 
