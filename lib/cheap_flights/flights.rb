51cc5639f2b9e264e01e24773107c85650b6dc56
Valena <valenadelvalle4@gmail.com>
2019-08-12 01:33:17 -0400

    commit

--- lib/cheap_flights/flights.rb
+++ lib/cheap_flights/flights.rb
@@ -11,16 +11,16 @@ class CheapFlights::Flights
     def self.scrape_flights
         flights = []
 
-        flights << self.scrape_googleflights 
-        # go to google, find the flight specials
+        flights << self.scrape_flightdeal
+        # go to theflightdeal, find the flight specials
         # extract the data 
         # instantiate a flight
 
         flights
     end
 
-    def self.scrape_googleflights
-        doc = Nokogiri::HTML(open("https://www.google.com/flights?hl=en#flt=/m/02_286..2019-08-27*./m/02_286.2019-08-31;c:USD;e:1;ls:1w;sd:0;t:h"))
+    def self.scrape_flightdeal
+        doc = Nokogiri::HTML(open("https://www.theflightdeal.com/category/flight-deals/nyc/"))
         binding.pry
     end
 end 
